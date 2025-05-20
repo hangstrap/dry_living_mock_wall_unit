dfa849cbfe92b44972e2b3a2086b6ec08907e50d
hangstrap <hangstrap@users.noreply.github.com>
2025-05-20 08:09:24 +0000

    deploy: 38c64b32804a0205ac87a26a311cf4cc1e1cb9fb

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1879233577"
+    serviceWorkerVersion: "508144749"
   }
 });
